d4e9930fb0083a3d74795627f629fa853e78e441
WandaAfriliaAyu <afriliawanda@gmail.com>
2021-12-08 20:44:39 +0700

    baru

---
+++ README.md
@@ -0,0 +1,24 @@
+# healthy_care
+
+Team ID: CSD-094
+
+Nama Anggota:
+1. Ravena - P299V4306
+2. Wanda Afrilia Ayu - P354V4387
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+   cek perubahan
+  
+cek kedua
---
+++ lib/screens/result.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:healthy_care/screens/BMI_result.dart';
+import 'package:healthy_care/recommendation.dart';
+import '../constants.dart';
+import '../reusable_card.dart';
+import '../bottom_button.dart';
+
+class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.interpretation,
+      @required this.bmiResult,
+      @required this.resultText});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: BoxContainer(
+              color: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                   Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          BottomButton(
+            buttonTitle: 'HASIL BMI',
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => hasil_BMI()),
+              );
+               },
+          ),
+          BottomButton( buttonTitle: "REKOMENDASI",
+          onTap: () {
+            Navigator.push(context,
+            MaterialPageRoute(builder: (context) => rekomen() ),
+            );
+          },)
+        ],
+      ),
+    );
+  }
+  }
