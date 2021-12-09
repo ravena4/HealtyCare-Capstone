48bbcbc2d8f3af39512c248da9085e0f3680c922
WandaAfriliaAyu <afriliawanda@gmail.com>
2021-12-09 16:36:09 +0700

    ada

--- lib/hasil_BMI.dart
+++ lib/hasil_BMI.dart
@@ -4,30 +4,6 @@ import 'package:healthy_care/rekomen.dart';
 
 class hasil_BMI extends StatelessWidget {
 
-// void main()
-// { 
-//   var teamA = 25; 
-//   var teamB = 18.5; 
-  
-//   if(teamA > teamB){
-//     print('Team A Menang');
-//   } else if(teamB > teamA){
-//     print('Team B Menang');
-//   } else{
-//     print('Kedua Team Seri');
-//   }
-// }
-
-// String getResult() {
-//     if (_bmi >= 25) {
-//       return 'Overweight';
-//     } else if (_bmi > 18.5) {
-//       return 'Normal';
-//     } else {
-//       return 'Underweight';
-//     }
-//   }
-
   final List makanan = [
     "Kondisi: BMR rendah menunjukkan angka BMR yang lebih kecil dari angka normal biasanya kurang dari 1.500 kkal per hari.",
     "Resiko: Hal yang dapat disebabkan oleh ketidaksimbangan hormon, seperti rendahnya hormon tiroid akibat tidak mendapatkan cukup kalori dalam jangka waktu yang lama. Akibatnya tubuh akan beradaptasi dengan kondisi tersebut. jika anda tidak memberi tubuh cukup kalori untuk beraktifitas, tubuh akan melakukan penyesuaian. Setelahnya, tubuh akan mengurangi penggunaan energi anda untuk bertahan hidup.",
